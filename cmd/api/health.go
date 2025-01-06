992a77989377b03cba1f5fbe8bf2ba31f6187d06
ngvanthanggit <thangitcbg@gmail.com>
2025-01-07 02:01:35 +0800

    add: displaying-errors methods

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -13,6 +13,6 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := writeJSON(w, http.StatusOK, data)
 	if err != nil {
-		writeJSONError(w, http.StatusInternalServerError, "err.Error()")
+		app.internalServerResponse(w, r, err)
 	}
 }
